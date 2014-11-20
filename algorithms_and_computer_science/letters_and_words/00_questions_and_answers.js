f4598b4d1359b1cd009f9622347bd99efd6d0a8e
Zee Spencer <zee@zeespencer.com>
2014-11-19 18:02:49 -0800

    Make letters and words more obvious what to do.

---
+++ algorithms_and_computer_science/letters_and_words/00_questions_and_answers.js
@@ -0,0 +1,10 @@
+function getAnswer(entry) {
+  // Put your code here.
+}
+
+
+if (!module.parent) {
+  console.log(getAnswer('Name: Zee') === 'Zee');
+  console.log(getAnswer('Favorite Color: Orange') === 'Orange');
+  console.log(getAnswer('Favorite Programming Language: Clojure') === 'Clojure');
+}
